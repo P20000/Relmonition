3c0d001bdcdffd20594f83adc6335a16fb80ca1f
p20000 <pranavdwivedi45645@gmail.com>
2026-04-08 19:17:06 +0530

    fix: resolve EKS auth error in github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,45 +8,14 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region = "ap-south-1"
 }
 
-# Create a Target Group for the EKS Cluster
-resource "aws_lb_target_group" "k8s_nodes" {
-  name     = "k8s-nodes-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
-}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
 
-# Create an Application Load Balancer
-resource "aws_lb" "relmonition_lb" {
-  name               = "relmonition-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = module.vpc.public_subnets
-}
-
-# Create a security group for the Load Balancer
-resource "aws_security_group" "lb_sg" {
-  name        = "relmonition-lb-sg"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+  name = "relmonition-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-south-1a", "ap-south-1b"]
@@ -57,27 +26,23 @@ resource "aws_security_group" "lb_sg" {
   single_nat_gateway = true
 }
 
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.0.0"
 
-  cluster_name    = "${var.project_name}-cluster"
+  cluster_name    = "relmonition-cluster"
   cluster_version = "1.30"
-  cluster_endpoint_public_access = true
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access = true
 
-  # This creates a managed node group with 1 t3.medium instance
   eks_managed_node_groups = {
     general = {
       min_size     = 1
       max_size     = 2
       desired_size = 1
-
       instance_types = ["t3.medium"]
     }
   }
