e67487ae0a5c7f0e7b85b37f1a23b64b2dbe6b06
p20000 <pranavdwivedi45645@gmail.com>
2026-05-06 11:33:41 +0530

    chore(infra): revert to stable Helm provider v2.17.0 and block-based HCL syntax

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,10 +67,10 @@ module "eks" {
 
 # Helm Provider configured to dynamically connect to our EKS Cluster
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec = {
+    exec {
       api_version = "client.authentication.k8s.io/v1"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command     = "aws"
