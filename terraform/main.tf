6fbca45b4e6537669e94591393c20dff07a81d20
p20000 <pranavdwivedi45645@gmail.com>
2026-05-20 09:57:28 +0530

    chore: organize documentation files into context folder and format main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,20 +3,20 @@ provider "aws" {
 }
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
-  name    = "relmonition-vpc"
-  cidr    = "10.0.0.0/16"
-  azs             = ["ap-south-1a", "ap-south-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  source             = "terraform-aws-modules/vpc/aws"
+  version            = "5.0.0"
+  name               = "relmonition-vpc"
+  cidr               = "10.0.0.0/16"
+  azs                = ["ap-south-1a", "ap-south-1b"]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24"]
   enable_nat_gateway = true
   single_nat_gateway = true
 }
 
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.8" 
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.8"
   cluster_name    = "relmonition-cluster"
   cluster_version = "1.30"
   vpc_id          = module.vpc.vpc_id
@@ -45,13 +45,13 @@ module "eks" {
     tenants = {
       instance_types = ["t3.medium"]
       capacity_type  = "SPOT" # Cost-effective for multi-tenant workloads
-      min_size     = 1
-      max_size     = 10
-      desired_size = 2
+      min_size       = 1
+      max_size       = 10
+      desired_size   = 2
 
       # Use Bottlerocket for HIPAA/Security compliance
       ami_type = "BOTTLEROCKET_x86_64"
-      
+
       labels = {
         role = "tenant-workload"
       }
